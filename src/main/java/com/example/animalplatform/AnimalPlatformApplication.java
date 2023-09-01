163af1f517b92ccd828e363ab49af38bb6690f5b
Susan <fasosan@gmail.com>
2023-09-01 13:46:10 +0900

    Initial commit

---
+++ src/main/java/com/example/animalplatform/AnimalPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.animalplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalPlatformApplication.class, args);
+    }
+
+}
