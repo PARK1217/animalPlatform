cb8914b26a1e50f9f1f62527c87ecf78b051da14
Susan <fasosan@gmail.com>
2023-09-06 09:27:20 +0900

    위로가기 버튼 삽입

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -5,3 +5,10 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
