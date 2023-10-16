0922aba7710ab83a820396759202f35aa33df319
Susan <fasosan@gmail.com>
2023-10-16 17:10:05 +0900

    회원가입 모달 & 로그인 js

--- src/main/resources/static/js/signIn.js
+++ src/main/resources/static/js/signIn.js
@@ -1,3 +1,38 @@
+
+function signInInit() {
+    signIn();
+}
+
+function signIn() {
+    var id = document.getElementById("id").value;
+    var password = document.getElementById("password").value;
+
+    if (id == "" || password == "") {
+        alert("아이디 또는 비밀번호를 입력하세요.");
+        return;
+    }
+
+    var data = {
+        id: id,
+        password: password
+    };
+
+    $.ajax({
+        type: "POST",
+        url: "/api/user/reg",
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        success: function (response) {
+            if (response["result"] == "success") {
+                alert("로그인에 성공하였습니다.");
+                window.location.href = "/";
+            } else {
+                alert("로그인에 실패하였습니다.");
+            }
+        }
+    });
+}
+
 // 로그인 상태 확인 변수 (예: true가 로그인된 상태, false가 로그아웃된 상태)
 var isLoggedIn = true;
 
